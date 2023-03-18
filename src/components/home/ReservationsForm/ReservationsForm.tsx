fd47aaf302c60d4416228fc1ff99aded3f57e843
Sonya Kozytska <sonya-ko@outlook.com>
2023-03-18 23:09:42 +0100

    edit mobile styles

--- src/components/home/ReservationsForm/ReservationsForm.tsx
+++ src/components/home/ReservationsForm/ReservationsForm.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import { useState } from 'react';
-import classes from './ReservationsForm.module.css';
-import TextField from '@mui/material/TextField';
-import { Dayjs } from 'dayjs';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { inherits } from 'util';
-import { availableHours } from '../../../static/restaurantData';
-import Button from '../../general/Button/Button';
+import React from "react";
+import { useState } from "react";
+import classes from "./ReservationsForm.module.css";
+import TextField from "@mui/material/TextField";
+import { styled } from "@mui/material/styles";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { inherits } from "util";
+import { availableHours } from "../../../static/restaurantData";
+import Button from "../../general/Button/Button";
 
-import arrow from '../../../assets/general/arrow-down-gray.png';
+import { Theme } from "@mui/material/styles";
+import createStyles from "@mui/material/styles";
+import makeStyles from "@mui/material/styles";
+
+import arrow from "../../../assets/general/arrow-down-gray.png";
 
 const ReservationsForm: React.FC = () => {
   const [formOpened, setFormOpened] = useState(true);
-  const [selectedTime, setSelectedTime] = useState('00:00 am');
+  const [selectedTime, setSelectedTime] = useState("00:00 am");
   const [selectedNumberGuests, setSelectedNumberGuests] = useState(1);
   const [reservationDate, setReservationDate] = React.useState<Date | null>(
-    new Date(2022, 3, 7),
+    new Date(2022, 3, 7)
   );
 
   const numberOfPersonsAvailable = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -30,10 +34,15 @@ const ReservationsForm: React.FC = () => {
     setSelectedNumberGuests(Number(event.target.value));
   };
 
-  const handleSubmit = (event: React.SyntheticEvent) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    console.log("button clicked");
     event.preventDefault();
     setFormOpened(false);
     console.log(selectedTime, selectedNumberGuests, reservationDate);
+  };
+
+  const makeNewReservation = () => {
+    setFormOpened(true);
   };
   return (
     <div className={classes.container}>
@@ -45,33 +54,39 @@ const ReservationsForm: React.FC = () => {
             <form
               onSubmit={handleSubmit}
               className={classes.formContainer}
-              action='#'
+              action="#"
             >
-              {/* <input type='date' /> */}
               <div className={classes.form}>
-                <LocalizationProvider
-                  className={classes.inputContainer}
-                  dateAdapter={AdapterDayjs}
-                >
-                  <DatePicker
-                    className={classes.datePicker}
-                    value={reservationDate}
-                    onChange={(newValue) => setReservationDate(newValue)}
-                    renderInput={(params: any) => (
-                      <TextField
-                        margin='none'
-                        style={{
-                          width: '30%',
-                          // border: '2px solid black',
-                          // minHeight: '200px',
-                          borderRadius: '0',
-                        }}
-                        className={classes.inputDate}
-                        {...params}
-                      />
-                    )}
-                  />
-                </LocalizationProvider>
+                <div className={classes.datePickerContainer}>
+                  <LocalizationProvider
+                    className={classes.inputContainer}
+                    dateAdapter={AdapterDayjs}
+                  >
+                    <DatePicker
+                      className={classes.datePicker}
+                      value={reservationDate}
+                      onChange={(newValue) => setReservationDate(newValue)}
+                      renderInput={(params: any) => (
+                        <TextField
+                          sx={{
+                            ".MuiInputBase-input": {
+                              fontFamily: "Lato, sans-serif;",
+                              lineHeight: "21px",
+                              borderColor: "#b5b8af",
+                            },
+                          }}
+                          margin="none"
+                          style={{
+                            width: "100%",
+                            borderRadius: "0",
+                          }}
+                          className={classes.inputDate}
+                          {...params}
+                        />
+                      )}
+                    />
+                  </LocalizationProvider>
+                </div>
                 <div className={classes.inputContainer}>
                   <select
                     onChange={handleTimeChange}
@@ -107,10 +122,11 @@ const ReservationsForm: React.FC = () => {
               </div>
 
               <Button
+                type="submit"
                 onClick={() => {}}
                 btnCentered={true}
-                btnStyle='green'
-                btnText='Book Now'
+                btnStyle="green"
+                btnText="Book Now"
               />
             </form>
           </>
@@ -120,10 +136,10 @@ const ReservationsForm: React.FC = () => {
               Thank you for submitting the form! We will get back to you soon!
             </p>
             <Button
-              onClick={() => {}}
+              onClick={makeNewReservation}
               btnCentered={true}
-              btnStyle='green'
-              btnText='Make a new reservation'
+              btnStyle="green"
+              btnText="Make a new reservation"
             />
           </>
         )}
